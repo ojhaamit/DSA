a627466704233529e51a72f5ed6386a721dc8033
Amit Ojha <ojhaamit@Amits-Laptop.local>
2024-09-30 14:46:36 -0700

    Initial commit for DSA practice in Java

---
+++ src/main/java/com/my/dsa/twopointers/TwoSum.java
@@ -0,0 +1,65 @@
+package com.my.dsa.twopointers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    /**
+     * Below is the brute force solution.
+     * Here, we are using two pointers (slow and fast) approach to iterate the loop, and
+     * see if the sum is matching the target sum or not.
+     * The time complexity of the solution is O(n2).
+     * The space complexity of the solution is O(1).
+     * @param nums
+     * @param target
+     * @return {i, j} the index of numbers that are adding up to target sum.
+     */
+    public int[] twoSumBruteForce(int[] nums, int target) {
+        if (nums.length == 0) {
+            // Return an empty array if no solution is found
+            return new int[0];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        // Return an empty array if no solution is found
+        return new int[0];
+    }
+
+    /**
+     * Below is the hash map solution.
+     * Here, we are storing elements into hash table and also checking if the compliment of the solution exists or not.
+     * The time complexity of the solution is O(n).
+     * The space complexity of the solution is O(n).
+     * @param nums
+     * @param target
+     * @return {i, j} the index of numbers that are adding up to target sum.
+     */
+    public int[] twoSumHashMap(int[] nums, int target) {
+        if (nums.length == 0) {
+            // Return an empty array if no solution is found
+            return new int[0];
+        }
+
+        Map<Integer, Integer> onePassMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (onePassMap.containsKey(complement)) {
+                return new int[] {onePassMap.get(complement), i};
+            }
+
+            onePassMap.put(nums[i], i);
+        }
+
+        // Return an empty array if no solution is found
+        return new int[0];
+    }
+}
---
+++ src/test/java/com/my/dsa/twopointers/TwoSumTest.java
@@ -0,0 +1,49 @@
+package test.java.com.my.dsa.twopointers;
+
+
+import com.my.dsa.twopointers.TwoSum;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+
+public class TwoSumTest {
+    private TwoSum twoSum;
+
+    @BeforeEach
+    public void setup() {
+        twoSum = new TwoSum();
+    }
+
+    @ParameterizedTest
+    @MethodSource("testDataProvider")
+    public void testBruteForceSolution(int[] expectedSolution, int[] nums, int targetSum) {
+        int[] solution = twoSum.twoSumBruteForce(nums, targetSum);
+
+        assertArrayEquals(expectedSolution, solution);
+    }
+
+    @ParameterizedTest
+    @MethodSource("testDataProvider")
+    public void testHashMapSolution(int[] expectedSolution, int[] nums, int targetSum) {
+        int[] solution = twoSum.twoSumHashMap(nums, targetSum);
+
+        assertArrayEquals(expectedSolution, solution);
+    }
+
+    public static Stream<Arguments> testDataProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{0, 1}, new int[]{2, 7, 11, 15}, 9),
+                Arguments.of(new int[]{1, 2}, new int[]{3, 2, 4}, 6),
+                Arguments.of(new int[]{0, 1}, new int[]{3, 3}, 6),
+                Arguments.of(new int[0], new int[]{3, 3}, 7),
+                Arguments.of(new int[0], new int[]{ }, 1)
+        );
+    }
+}
